196e8c1f7e4caea3a3f95473894ba23926d38ad0
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2024-03-20 23:11:02 +0530

    complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,12 +2,12 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const users_router = express.Router();
 const axios = require('axios');
 
 //  Task 6
 //  Register a new user
-public_users.post("/register", (req,res) => {
+users_router.post("/register", (req,res) => {
     const { username, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({ message: "Username and password are required" });
@@ -16,7 +16,7 @@ public_users.post("/register", (req,res) => {
       return res.status(409).json({ message: "Username already exists" });
     }
     users.push({ username, password });
-    return res.status(201).json({ message: "User registered successfully" });
+    return res.status(201).json({ message: "User registered success" });
 });
 
 //  Task10
@@ -29,13 +29,13 @@ const getBooks = () => {
 
 //  Task 1
 //  Get the book list available in the shop
-public_users.get('/',async function (req, res) {
+users_router.get('/',async function (req, res) {
   try {
     const bookList = await getBooks(); 
     res.json(bookList); // Neatly format JSON output
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error retrieving book list" });
+    res.status(500).json({ message: "cant get book list" });
   }
 });
 
@@ -54,7 +54,7 @@ const getByISBN = (isbn) => {
 
 //  Task 2
 //  Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+users_router.get('/isbn/:isbn',function (req, res) {
     getByISBN(req.params.isbn)
     .then(
         result => res.send(result),
@@ -64,7 +64,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
 
 //  Task 3 & Task 12
 //  Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+users_router.get('/author/:author',function (req, res) {
     const author = req.params.author;
     getBooks()
     .then((bookEntries) => Object.values(bookEntries))
@@ -74,7 +74,7 @@ public_users.get('/author/:author',function (req, res) {
 
 //  Task 4 & Task 12
 //  Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+users_router.get('/title/:title',function (req, res) {
     const title = req.params.title;
     getBooks()
     .then((bookEntries) => Object.values(bookEntries))
@@ -84,7 +84,7 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Task 5 & Task 13
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+users_router.get('/review/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     getByISBN(req.params.isbn)
     .then(
@@ -93,4 +93,4 @@ public_users.get('/review/:isbn',function (req, res) {
     );
 });
 
-module.exports.general = public_users;
+module.exports.general = users_router;
